3245b81ed2742e366f5a2fb8a7582e59ec11bb65
Mohit <mohit-sharma@live.com>
2014-08-23 16:46:25 +0700

    fixed URL patterns and CSS

--- djangotodo/urls.py
+++ djangotodo/urls.py
@@ -14,7 +14,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^todo/', include(admin.site.urls)),
+    url(r'^add/', include(admin.site.urls)),
+    url(r'^new/', include(admin.site.urls)),
     url(r'^$', 'todo.views.index'),
 )
 urlpatterns += staticfiles_urlpatterns()
